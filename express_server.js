c2e4092ae2a05296f83a0de0f8eaeb77fa12a2ea
t5krishn <tk95@live.ca>
2019-07-08 19:06:47 +0000

    Added form page template: urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,9 +36,18 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);
+  let templateVars = {urls: urlDatabase};
+  res.send('Ok');
+});
+
