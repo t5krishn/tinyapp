bcec2114ac65a3e0e42ec32f8280aabd7ce23db9
t5krishn <tk95@live.ca>
2019-07-08 17:42:12 +0000

    Added /urls path using ejs and sending in the database object

--- express_server.js
+++ express_server.js
@@ -22,6 +22,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
