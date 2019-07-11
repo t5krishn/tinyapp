65f0a383ab0e80771dfccb1eb96584dd8b40c916
t5krishn <tk95@live.ca>
2019-07-11 19:07:32 +0000

    Extracted classes for url, user and visitor

---
+++ classes.js
@@ -0,0 +1,59 @@
+const { generateRandomString } = require('./helper');
+
+// Class used to create URLS
+class URL {
+  constructor(longURL, userID){
+    this.longURL = longURL;
+    this.userID = userID;
+    this.createdOn = new Date();
+    this.visits = {
+      count : 0,
+      eachVisit : [],
+      uniqueVisitors : []
+    }
+  };
+  
+  incrementCount() {
+    this.visits.count = this.visits.count + 1;
+  };
+
+  getUniqueVisits() {
+    return this.visits.uniqueVisitors.length;
+  };
+
+  getVisits() {
+    return this.visits.eachVisit;
+  };
+
+  addVisit(visitorId, time) {
+    this.visits.eachVisit.push({
+      visitorId,
+      time
+    });
+    this.incrementCount();
+  };
+
+  addUnique(visitorID) {
+    this.visits.uniqueVisitors.push(visitorID);
+  };
+
+};
+
+
+// Class used to create new Users
+class User {
+  constructor(email, password) {
+    this.id = generateRandomString();
+    this.email = email;
+    this.password = password;
+  }
+};
+
+// Class for each unique visitor
+class Visitor {
+  constructor(id){
+    this.visitorId = id;
+  }
+};
+
+module.exports = { User, Visitor, URL };
